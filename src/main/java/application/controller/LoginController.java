7a35e9e56e5adae154b0acdc9b6b30260d8c5489
cb-angalaparameashwari <angalaparameashwari@gmail.com>
2019-09-20 10:57:32 +0530

    index page added

--- src/main/java/application/controller/LoginController.java
+++ src/main/java/application/controller/LoginController.java
@@ -2,15 +2,23 @@ package application.controller;
 
 
 import application.bean.Admin;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
     public String getloginpage(Admin admin) {
+
         return "login.html";
+    }
+
+    @PostMapping("/login")
+    public String processForm(Admin admin) {
+
+        return "details.html";
     }
 
 }
