9c2fe3c6cdda013a303ee0cc2a04095c8efc4802
cb-angalaparameashwari <angalaparameashwari@gmail.com>
2019-09-23 11:31:16 +0530

    login page

--- src/main/java/application/controller/LoginController.java
+++ src/main/java/application/controller/LoginController.java
@@ -2,23 +2,24 @@ package application.controller;
 
 
 import application.bean.Admin;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 @Controller
+//@SessionAttributes("userName")
+@ComponentScan("com.howtodoinjava.web")
 public class LoginController {
 
     @GetMapping("/login")
     public String getloginpage(Admin admin) {
 
-        return "login.html";
+        return "login";
     }
 
     @PostMapping("/login")
     public String processForm(Admin admin) {
-
-        return "details.html";
+        return "details";
     }
 
 }
